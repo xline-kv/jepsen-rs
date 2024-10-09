1535a5e5025583e98bc20adedffc0bc38c727546
lxl66566 <lxl66566@gmail.com>
2024-10-09 09:17:19 +0800

    feat(nemesis): add partition_random_n and leader_send_to_majority_but_cannot_receive
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

--- src/nemesis/mod.rs
+++ src/nemesis/mod.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 
+use madsim::rand::Rng;
 use serde::{Deserialize, Serialize};
 
 use crate::utils::{select_numbers_from_range, OverflowingAddRange};
@@ -43,21 +44,35 @@ pub trait NemesisCluster {
     async fn pause(&self, servers: &[ServerId]);
     async fn resume(&self, servers: &[ServerId]);
     async fn get_leader_without_term(&self) -> ServerId;
-    /// Note: clog link twice is allowed and will not generate error.
-    fn clog_link(&self, fst: ServerId, snd: ServerId);
-    /// Note: unclog link twice is allowed and will not generate error.
-    fn unclog_link(&self, fst: ServerId, snd: ServerId);
+    /// clog link for both side.
+    fn clog_link_both(&self, fst: ServerId, snd: ServerId);
+    /// unclog link for both side.
+    fn unclog_link_both(&self, fst: ServerId, snd: ServerId);
+    /// clog link for one side.
+    fn clog_link_single(&self, fst: ServerId, snd: ServerId);
+    /// unclog link for one side.
+    fn unclog_link_single(&self, fst: ServerId, snd: ServerId);
     fn size(&self) -> usize;
+
     fn majority(&self) -> usize {
         self.size() / 2 + 1
     }
-    fn clog_or_resume_link(&self, fst: ServerId, snd: ServerId, resume: bool) {
+    fn clog_or_resume_link_both(&self, fst: ServerId, snd: ServerId, resume: bool) {
         debug_assert!(fst < self.size() as u64);
         debug_assert!(snd < self.size() as u64);
         if resume {
-            self.unclog_link(fst, snd)
+            self.unclog_link_both(fst, snd)
         } else {
-            self.clog_link(fst, snd)
+            self.clog_link_both(fst, snd)
+        }
+    }
+    fn clog_or_resume_link_single(&self, fst: ServerId, snd: ServerId, resume: bool) {
+        debug_assert!(fst < self.size() as u64);
+        debug_assert!(snd < self.size() as u64);
+        if resume {
+            self.unclog_link_single(fst, snd)
+        } else {
+            self.clog_link_single(fst, snd)
         }
     }
 }
@@ -83,14 +98,30 @@ pub trait Nemesis: NemesisCluster {
         resume: bool,
     ) {
         let set1 = servers.into();
+        assert!(
+            set1.len() < self.size(),
+            "set1 must be smaller than cluster size"
+        );
         let total: HashSet<_> = (0..self.size()).map(|x| x as u64).collect();
         let set2: HashSet<_> = total.difference(&set1).collect();
         for x in set1.iter() {
             for y in set2.iter() {
-                self.clog_or_resume_link(*x, **y, resume);
+                self.clog_or_resume_link_both(*x, **y, resume);
             }
         }
     }
+
+    /// randomly select `n` servers to be partitioned
+    async fn partition_random_n(&self, n: usize) {
+        assert!(n < self.size(), "n must be smaller than cluster size");
+        let mut part = HashSet::with_capacity(n);
+        while part.len() < n {
+            part.insert(madsim::rand::thread_rng().gen_range(0..self.size()) as ServerId);
+        }
+        dbg!(&part);
+        self.partition_halves(part).await
+    }
+
     /// Partition the cluster into a ring-like relation. Each node is able to
     /// connect to majority.
     ///
@@ -121,7 +152,7 @@ pub trait Nemesis: NemesisCluster {
             let to_be_clogged = total.difference(&expected);
             to_be_clogged.for_each(|x| {
                 if *x >= i {
-                    self.clog_link(i as u64, *x as u64)
+                    self.clog_link_both(i as u64, *x as u64)
                 }
             });
         }
@@ -135,7 +166,18 @@ pub trait Nemesis: NemesisCluster {
             .map(|x| x as u64)
             .filter(|x| x != &leader)
             .take(self.majority() - 1)
-            .for_each(|x| self.clog_link(leader, x))
+            .for_each(|x| self.clog_link_both(x, leader))
+    }
+
+    /// This is a one-direction partition, make leader could send message to
+    /// majority, but cannot receive the message from majority.
+    async fn leader_send_to_majority_but_cannot_receive(&self) {
+        let leader = self.get_leader_without_term().await;
+        (0..self.size())
+            .map(|x| x as u64)
+            .filter(|x| x != &leader)
+            .take(self.majority() - 1)
+            .for_each(|x| self.clog_link_single(x, leader))
     }
 }
 
@@ -143,7 +185,10 @@ impl<T: NemesisCluster> Nemesis for T {}
 
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashMap, sync::Mutex};
+    use std::{
+        collections::{HashMap, VecDeque},
+        sync::Mutex,
+    };
 
     use tap::Tap;
 
@@ -190,6 +235,18 @@ mod tests {
                     .collect()
             );
         }
+
+        /// Get all reachable nodes from `start`, using bfs.
+        fn get_all_reachable_nodes(&self, start: ServerId) -> HashSet<ServerId> {
+            let mut res = HashSet::new();
+            let l = self.connections.lock().unwrap();
+            let mut q = VecDeque::from([start]);
+            while let Some(x) = q.pop_front() {
+                res.insert(x);
+                q.extend(l.get(&x).unwrap().iter().filter(|x| !res.contains(x)));
+            }
+            res
+        }
     }
 
     #[async_trait::async_trait]
@@ -209,15 +266,23 @@ mod tests {
         async fn get_leader_without_term(&self) -> ServerId {
             0
         }
-        fn clog_link(&self, fst: ServerId, snd: ServerId) {
+        fn clog_link_both(&self, fst: ServerId, snd: ServerId) {
             let mut l = self.connections.lock().unwrap();
             l.get_mut(&fst).unwrap().remove(&snd);
             l.get_mut(&snd).unwrap().remove(&fst);
         }
-        fn unclog_link(&self, fst: ServerId, snd: ServerId) {
+        fn unclog_link_both(&self, fst: ServerId, snd: ServerId) {
             let mut l = self.connections.lock().unwrap();
             l.get_mut(&fst).unwrap().insert(snd);
             l.get_mut(&snd).unwrap().insert(fst);
+        }
+        fn clog_link_single(&self, fst: ServerId, snd: ServerId) {
+            let mut l = self.connections.lock().unwrap();
+            l.get_mut(&fst).unwrap().remove(&snd);
+        }
+        fn unclog_link_single(&self, fst: ServerId, snd: ServerId) {
+            let mut l = self.connections.lock().unwrap();
+            l.get_mut(&fst).unwrap().insert(snd);
         }
         fn size(&self) -> usize {
             self.connections.lock().unwrap().len()
@@ -242,6 +307,19 @@ mod tests {
             .partition_halves((0..=2).collect::<HashSet<_>>())
             .await;
         cluster.assert_eq(vec![vec![1, 2], vec![0, 2], vec![0, 1], vec![4], vec![3]]);
+
+        let cluster = MockCluster::new(6);
+        cluster
+            .partition_halves((0..=2).collect::<HashSet<_>>())
+            .await;
+        cluster.assert_eq(vec![
+            vec![1, 2],
+            vec![0, 2],
+            vec![0, 1],
+            vec![4, 5],
+            vec![3, 5],
+            vec![3, 4],
+        ]);
     }
 
     #[madsim::test]
@@ -274,5 +352,25 @@ mod tests {
             .count();
         assert!(leader_connections_num < cluster.majority());
         dbg!(cluster.connections.lock().unwrap().get(&leader).unwrap());
+    }
+
+    #[madsim::test]
+    async fn test_partition_random_n() {
+        let cluster = MockCluster::new(6);
+        cluster.partition_random_n(3).await;
+        assert_eq!(cluster.get_all_reachable_nodes(0).len(), 3);
+    }
+
+    #[madsim::test]
+    async fn test_leader_send_to_majority_but_cannot_receive() {
+        let cluster = MockCluster::new(5);
+        cluster.leader_send_to_majority_but_cannot_receive().await;
+        let leader = cluster.get_leader_without_term().await;
+        let leader_connections_num = (0..cluster.size())
+            .map(|x| x as u64)
+            .filter(|x| *x != leader)
+            .filter(|x| cluster.connected_one_way(*x, leader))
+            .count();
+        assert!(leader_connections_num < cluster.majority());
     }
 }
